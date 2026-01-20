bd92181a83cd8f52a0ce9862f7dc9cca8f292f03
Michael Zeta <michael@a2zeta.com>
2026-01-19 17:33:50 -0800

    Uses plain script tag for SEO instead of next/Script

--- src/components/Schema/MusicRecording.tsx
+++ src/components/Schema/MusicRecording.tsx
@@ -40,7 +40,7 @@ export const MusicRecordingSchema = ({ song }: { song: Song }) => {
   }
 
   return (
-    <Script
+    <script
       id={`music-schema-${song.id}`}
       type="application/ld+json"
       dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
